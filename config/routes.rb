641e7ac0b76e880af3df21c402386e38768b7485
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-06-20 00:27:42 +0000

    Set up model

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Nomster::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'places#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
