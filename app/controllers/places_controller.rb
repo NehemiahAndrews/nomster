908a35091ac519dcb6e07328fc3f9d28c47afa3f
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-06-22 23:54:11 +0000

    Enabled Place Editing

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -24,7 +24,9 @@ class PlacesController < ApplicationController
 	end
 
 	def update
-		@place = Place.update(params[:id])
+		@place = Place.find(params[:id])
+		@place.update_attributes(place_params)
+		redirect_to root_path
 	end
 
 	private
