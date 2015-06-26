88361d5611de7b4cb908de53c9ee974a1a6bdb78
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-06-26 23:35:52 +0000

    Added mailer

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,10 @@
+class NotificationMailer < ActionMailer::Base
+  default from: "no-reply@nomsterapp.com"
+
+  def comment_added
+    mail(to: 'nehemiah.andrews@gmail.com',
+      subject: "A comment has been added to your place.")
+  end
+
+
+end
