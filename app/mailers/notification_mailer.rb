0153aa8ce1dce9a583c4f3b347249eb8c3e4dc13
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-06-28 10:46:38 +0000

    Enabled emailing

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,11 @@
 class NotificationMailer < ActionMailer::Base
   default from: "nehemiah.andrews@gmail.com"
 
-  def comment_added
-    mail(to: 'nehemiah.andrews@gmail.com',
-      subject: "A comment has been added to your place.")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+      subject: "A comment has been added to #{@place.name}")
   end
 
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Nomster::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3030'}
+
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
