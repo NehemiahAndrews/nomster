c8d13e67070827c47e9f13785fc00ccc14eaf24b
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-07-02 00:02:30 +0000

    Added User Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Nomster::Application.routes.draw do
   devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  
   root 'places#index'
 
   resources :places do
     resources :comments, :only => :create
     resources :photos, :only => :create
   end
+
+  resources :users, :only => :show
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
