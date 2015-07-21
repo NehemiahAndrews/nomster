cc27128c9d4dd9208c1e36eac2ed7697108c21db
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-07-21 01:05:28 +0000

    Added Comments and User Dashboard Tests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,9 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
 	def create
-    @place = Place.find(params[:place_id])
-    @place.comments.create(comment_params.merge(:user => current_user))
-    redirect_to place_path(@place)
+    
+    current_place.comments.create(comment_params.merge(:user => current_user))
+    redirect_to place_path(current_place)
 	end
 
   private
@@ -13,5 +13,9 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:message,:rating)
   end
 
+  helper_method :current_place
+  def current_place
+    @current_place ||= Place.find_by_id(params[:place_id])
+  end
 end
 
--- test/factories.rb
+++ test/factories.rb
@@ -3,4 +3,19 @@ FactoryGirl.define do
     message "Absolutely terrible" 
     rating "1_star"
   end
+
+  factory :user do
+    email "nehemiah.andrews@gmail.com"
+    password "12345!!!54321"
+    password_confirmation "12345!!!54321"
+  end
+
+  factory :place do
+    name "Home"
+    description "There's no place like it"
+    address "23127 Violet St, Farmington, MI 48336"
+    latitude "42.461418"
+    longitude "-83.355656"
+  end
+
 end
